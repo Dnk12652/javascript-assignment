0a5165fb6054952b3928935bcaf504aa96c90b5f
Dnk12652 <devadurgam52@gmail.com>
2022-01-18 11:22:07 +0530

    can_drive_ass

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,4 +1,18 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
+    if (hasDrivingLiscence === false){
+        return  "You cannot drive"
+
+    }else if ((hasDrivingLiscence===true)&&((isTired ===true)&& (isSober===false))){
+        return "You shouldn't drive"
+    }else if ((hasDrivingLiscence===true)&&((isTired===false)&& (isSober===true))){
+        return "You can drive"
+    }else{
+        return "You shouldn't drive"
+
+    }
+
+
+
     // Write you code here
 }
 
