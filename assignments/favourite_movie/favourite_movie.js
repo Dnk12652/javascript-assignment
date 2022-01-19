c2eaf0656143cc403ae1b4db5214f95678b8d7cc
Dnk12652 <devadurgam52@gmail.com>
2022-01-19 13:16:08 +0530

    favourite_movie_ass

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -1,6 +1,13 @@
 const movies = [];
 
 function favouriteMovie(operation, movie) {
+    if (operation === "add"){
+        movies.push(movie)
+    }else if (operation === "remove"){
+        movies.pop()
+    }
+    return movies
+    
     // Write your code here
 }
 
