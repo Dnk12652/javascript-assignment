4c0f7093c8577fdf13cde8111a5647a9600221fd
Dnk12652 <devadurgam52@gmail.com>
2022-01-23 09:35:24 +0530

    create_object

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,11 @@
 function CreateObject(arr) {
+    var dic = {}
+    for (var i=0;i<arr.length;i+=2){
+        dic[arr[i]]=arr[i+1]
+    }
+    return dic
+  
     // Write your code here
-}
+} 
 
 module.exports = CreateObject;
