6bfcb6665ad3d1a763f108da8ddc070cfecd8c24
Dnk12652 <95068562+Dnk12652@users.noreply.github.com>
2022-01-31 15:27:06 +0530

    Update create_object.js
    check now

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
-function CreateObject(arr) {
-    // Write your code here
+function CreateObject(arr) { 
+    var dic={}
+    for (var i=0;i<arr.length;i+=2){
+        dic[arr[i]]=arr[i+1]
+    }
+    return dic
+    
 }
 
 module.exports = CreateObject;
